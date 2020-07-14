4e98c44af8298eada54bc03013a82d4c7155d17b
Animatronic Dinosaurs <66761229+artificialdinosaur@users.noreply.github.com>
2020-07-14 12:34:10 +0800

    1

---
+++ wp-content/uploads/cache/wpml/twig/8c/8cd693a162b4347dfd28857b2c4f1911e640b9f53c23ab7d401d1f49f56a205a.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace WPML\Core;
+
+use \WPML\Core\Twig\Environment;
+use \WPML\Core\Twig\Error\LoaderError;
+use \WPML\Core\Twig\Error\RuntimeError;
+use \WPML\Core\Twig\Markup;
+use \WPML\Core\Twig\Sandbox\SecurityError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedTagError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFilterError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use \WPML\Core\Twig\Source;
+use \WPML\Core\Twig\Template;
+
+/* save-notification.twig */
+class __TwigTemplate_119e4fd05a34895d6ac770a3e527c150c14526fc85def341f88d62652da99d54 extends \WPML\Core\Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<span class=\"js-wpml-ls-messages wpml-ls-messages\"></span><span class=\"spinner\"></span>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "save-notification.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  32 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "save-notification.twig", "/www/wwwroot/dinosaurjoy.com/wp-content/plugins/sitepress-multilingual-cms/templates/language-switcher-admin-ui/save-notification.twig");
+    }
+}
